7ee39bee8aee4be329c0bfc9b52f60d25b72e0a1
HelloYanming <112279702+HelloYanming@users.noreply.github.com>
2023-03-05 18:49:05 -0500

    Update Web.py

--- Web.py
+++ Web.py
@@ -444,7 +444,7 @@ plot4 = alt.Chart(subset).transform_joinaggregate(
 ).properties(
     title="Diabetes Rate Under Certain Disease"
 ).properties(
-    width=alt.Step(10) 
+    #width=alt.Step(10) 
 )
 
 # plot4
